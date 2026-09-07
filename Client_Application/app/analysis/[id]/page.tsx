980c4b0f6f33d8993b529bf9e74fa94d055518be
Dhawal-Menaria <dhawalmenaria28@gmail.com>
2026-09-07 11:59:25 +0530

    fixed some minor version diff bugs ready for development now

--- Client_Application/app/analysis/[id]/page.tsx
+++ Client_Application/app/analysis/[id]/page.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { DashboardShell } from "@/components/dashboard/DashboardShell";
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
 
 interface AnalysisPageProps {
   params: Promise<{
